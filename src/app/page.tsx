200a3237b2381660476ebf046477cf901869f419
Văleanu Lucian-George <valeanuluciangeorge@gmail.com>
2025-07-11 18:03:17 +0300

    Added bachelor thesis on website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+"use client"
+
+import React, { useState } from 'react';
 import TitleCard from './components/title-card/title-card';
+import './page.module.css';
 
 export default function Home() {
+
   return (
     <TitleCard />
   );
